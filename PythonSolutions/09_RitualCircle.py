321a903fef48f496580f10e1bad6ba5f40422de6
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-03-02 17:15:01 +0200

    feat(09): Create PYTHON solution for 09 task

---
+++ PythonSolutions/09_RitualCircle.py
@@ -0,0 +1,22 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    T = int(data[0])
+    idx = 1
+    result = []
+
+    for _ in range(T):
+        N = int(data[idx])
+        idx += 1
+        result.append(str(factorial(N - 1)))
+
+    print("\n".join(result))
+
+if __name__ == "__main__":
+    main()
