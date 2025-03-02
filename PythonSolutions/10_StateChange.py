a4e6c5385dd98cb03ea003b079df568eff705c3a
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-03-02 17:15:46 +0200

    feat(10): Create PYTHON solution for 10 task

---
+++ PythonSolutions/10_StateChange.py
@@ -0,0 +1,21 @@
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    T = int(data[0])
+    idx = 1
+    result = []
+
+    for _ in range(T):
+        X = int(data[idx])
+        N = int(data[idx + 1])
+        idx += 2
+
+        mask = 1 << N
+        state = X ^ mask
+        result.append(str(state))
+
+    print("\n".join(result))
+
+if __name__ == "__main__":
+    main()
