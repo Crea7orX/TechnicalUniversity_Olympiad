7229f2420518f50a3a54121463eb72c055b587b7
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-03-02 16:48:27 +0200

    feat(12): Create solution for 12 task

---
+++ src/EmergencyCenters_12/EmergencyCenters.java
@@ -0,0 +1,38 @@
+package EmergencyCenters_12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EmergencyCenters {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(reader.readLine().trim());
+        StringBuilder result = new StringBuilder();
+
+        int[][] A = new int[N][N];
+        for (int n = 0; n < N; n++) {
+            String[] input = reader.readLine().trim().split("\\s+");
+            for (int i = 0; i < input.length; i++) {
+                A[n][i] = Integer.parseInt(input[i]);
+            }
+        }
+
+        int minDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < N - 3; i++) {
+            for (int j = i + 1; j < N - 2; j++) {
+                for (int k = j + 1; k < N - 1; k++) {
+                    for (int l = k + 1; l < N; l++) {
+                        minDistance = Math.min(minDistance,
+                                A[i][j] + A[i][k] + A[i][l]
+                                        + A[j][k] + A[j][l] + A[k][l]);
+                    }
+                }
+            }
+        }
+        result.append(minDistance).append("\n");
+
+        System.out.print(result);
+    }
+}
