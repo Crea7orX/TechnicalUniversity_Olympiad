81db0bf25d7c75098e96f3367aa649d0c1553f95
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-04-02 13:14:35 +0300

    feat: Create template for files

---
+++ src/TEMPLATE/Main.java
@@ -0,0 +1,18 @@
+package TEMPLATE;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(reader.readLine().trim());
+        StringBuilder result = new StringBuilder();
+
+        for (int t = 0; t < T; t++) {}
+
+        System.out.print(result);
+    }
+}
